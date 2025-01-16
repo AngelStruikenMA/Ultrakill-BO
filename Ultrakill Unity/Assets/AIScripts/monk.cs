bbd6fcaa58f168d8eb5909ff8b5a7459a5d20a2a
AngelStruikenMA <29418@ma-web.nl>
2025-01-16 09:56:04 +0100

    EnemyAnimation

--- Ultrakill Unity/Assets/AIScripts/monk.cs
+++ Ultrakill Unity/Assets/AIScripts/monk.cs
@@ -14,7 +14,7 @@ public class monk : MonoBehaviour
         private float attackTimer = 0f;             
 
    
-    public Animator animator;                  
+    private Animator animator;                  
 
     void Start()
     {
@@ -26,7 +26,7 @@ public class monk : MonoBehaviour
             Debug.LogError("Player not found. Ensure the player is tagged 'Player'.");
         }
 
-    
+        animator = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
     }
 
@@ -63,15 +63,20 @@ public class monk : MonoBehaviour
 
     private void TryAttackPlayer()
     {
+         if (animator != null)
+            {
+               
+            
         if (attackTimer >= attackCooldown)
         {
+            animator.SetTrigger("Attack");
             attackTimer = 0f;
+             
 
         
-            if (animator != null)
-            {
-                animator.SetTrigger("Attack");
-            }
+            
+                
+            
 
             
             Health1 playerHealth = player.GetComponent<Health1>();
@@ -80,7 +85,9 @@ public class monk : MonoBehaviour
                 playerHealth.TakeDamage(attackDamage);
             }
         }
+            }
     }
+        
 
     private void OnDrawGizmosSelected()
     {
